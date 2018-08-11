f2a359d320fceacd6b2711861c1fa10e9058c7e7
Leigh Garbs <leighgarbs@gmail.com>
2018-08-11 17:42:26 -0400

    WIP

--- vars/doStage.groovy
+++ vars/doStage.groovy
@@ -2,7 +2,7 @@
 
 def call(stageName, stageBody, stageArgs = [])
 {
-  echo stageBody.getClass().toString()
+  echo stageBody.getClass().getSuperclass().toString()
   stage (stageName)
   {
     gitlabCommitStatus(name: stageName)
