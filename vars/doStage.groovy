a4803c821ef66549cc85cf439a363feebbc14612
Leigh Garbs <leighgarbs@gmail.com>
2018-08-11 17:38:59 -0400

    Wip

--- vars/doStage.groovy
+++ vars/doStage.groovy
@@ -2,7 +2,7 @@
 
 def call(stageName, stageBody, stageArgs = [])
 {
-  echo stageBody.getClass()
+  echo stageBody.getClass().toString()
   stage (stageName)
   {
     gitlabCommitStatus(name: stageName)
