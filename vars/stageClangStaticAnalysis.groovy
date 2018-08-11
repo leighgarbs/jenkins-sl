03a0e8a1ae8cc93a23488d0bc9f687bbc1715ca7
Leigh Garbs <leighgarbs@gmail.com>
2018-08-11 16:26:24 -0400

    WIP

---
+++ vars/stageClangStaticAnalysis.groovy
@@ -0,0 +1,15 @@
+#!groovy
+
+def call(args)
+{
+  cleanUp()
+
+  sh '''
+    cd workdir
+    scan-build ../bin/run-cmake --debug .
+    scan-build -o clangScanBuildReports -v -v --use-cc clang \
+      --use-analyzer=/usr/bin/clang make -B
+  '''
+
+  saveArtifacts()
+}
