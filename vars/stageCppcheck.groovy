419ba0d7cd539e1433343ab4eae983a605d4915d
Leigh Garbs <leighgarbs@gmail.com>
2018-08-11 15:30:23 -0400

    WIP

---
+++ vars/stageCppcheck.groovy
@@ -0,0 +1,27 @@
+#!groovy
+
+def call()
+{
+  cleanUp()
+
+  dir('workdir')
+  {
+    def shellReturnStatus = sh returnStatus: true, script: '''
+      ../bin/run-cppcheck -J --suppress=unusedFunction .
+    '''
+
+    setUnstableOnShellResult(shellReturnStatus, 1)
+
+    publishCppcheck displayAllErrors: false,
+                    displayErrorSeverity: true,
+                    displayNoCategorySeverity: true,
+                    displayPerformanceSeverity: true,
+                    displayPortabilitySeverity: true,
+                    displayStyleSeverity: true,
+                    displayWarningSeverity: true,
+                    pattern: 'cppcheck-result.xml',
+                    severityNoCategory: false
+  }
+
+  saveArtifacts()
+}
