83657974034a90916b528c501649adc71799a6d5
Leigh Garbs <leighgarbs@gmail.com>
2018-08-11 16:02:05 -0400

    WIP

---
+++ vars/stageBuild.groovy
@@ -0,0 +1,17 @@
+#!groovy
+
+def stageBuild(args)
+{
+  cleanUp()
+
+  withEnv(['BUILD_TYPE=' + args[0], 'TARGET=' + args[1]])
+  {
+    sh '''
+      cd workdir
+      ../bin/run-cmake --$BUILD_TYPE .
+      make -B $TARGET
+    '''
+  }
+
+  saveArtifacts()
+}
