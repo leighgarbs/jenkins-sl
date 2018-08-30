a3c38b5376e6be28dd1558756bec45e3b299ae05
Leigh Garbs <leighgarbs@gmail.com>
2018-08-29 21:45:12 -0400

    cppcheck-args

--- vars/stageCppcheck.groovy
+++ vars/stageCppcheck.groovy
@@ -6,20 +6,29 @@ def call(args)
 
   dir('workdir')
   {
-    def shellReturnStatus = sh returnStatus: true, script: '''
-      ../bin/run-cppcheck -J .
-    '''
+    cppcheck_args = args[0]
+    if (cppcheck_args == null)
+    {
+      cppcheck_args = ""
+    }
 
-    setUnstableOnShellResult(shellReturnStatus, 1)
+    withEnv(['CPPCHECK_ARGS=' + cppcheck_args])
+    {
+      def shellReturnStatus = sh returnStatus: true, script: '''
+        ../bin/run-cppcheck -J $CPPCHECK_ARGS .
+      '''
 
-    publishCppcheck displayAllErrors: false,
-                    displayErrorSeverity: true,
-                    displayNoCategorySeverity: true,
-                    displayPerformanceSeverity: true,
-                    displayPortabilitySeverity: true,
-                    displayStyleSeverity: true,
-                    displayWarningSeverity: true,
-                    pattern: 'cppcheck-result.xml',
-                    severityNoCategory: false
+      setUnstableOnShellResult(shellReturnStatus, 1)
+
+      publishCppcheck displayAllErrors: false,
+                      displayErrorSeverity: true,
+                      displayNoCategorySeverity: true,
+                      displayPerformanceSeverity: true,
+                      displayPortabilitySeverity: true,
+                      displayStyleSeverity: true,
+                      displayWarningSeverity: true,
+                      pattern: 'cppcheck-result.xml',
+                      severityNoCategory: false
+    }
   }
 }
