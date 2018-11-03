c81c5b1303393c749058f95da90d1225ff267ab0
Leigh Garbs <leighgarbs@gmail.com>
2018-11-03 18:42:31 -0400

    stage-errorcall-fix

--- vars/doStage.groovy
+++ vars/doStage.groovy
@@ -1,33 +1,42 @@
 #!groovy
 
-def call(stageName, stageBody, stageArgs = [])
+def call(name, body, args = [])
 {
-  stage (stageName)
+  stage (name)
   {
-    gitlabCommitStatus(name: stageName)
+    timestamps
     {
-      try
+      gitlabCommitStatus(name: name)
       {
-        stageBody(stageArgs)
+        try
+        {
+          body(args)
+        }
+        catch (err)
+        {
+          // Any exception fails the build
+          currentBuild.result = 'FAILURE'
+        }
+        finally
+        {
+          saveArtifacts()
+        }
       }
-      catch (err)
-      {
-        currentBuild.result = 'FAILURE'
-      }
-      finally
-      {
-        saveArtifacts()
-      }
-    }
 
-    if (currentBuild.result == 'UNSTABLE' ||
-        currentBuild.result == 'FAILURE')
-    {
-      updateGitlabCommitStatus(name: stageName, state: 'failed')
-
-      if (currentBuild.result == 'FAILURE')
+      if (currentBuild.result == 'UNSTABLE' ||
+          currentBuild.result == 'FAILURE')
       {
-        error
+        // Gitlab doesn't have a commit status for unstable
+        updateGitlabCommitStatus(name: name, state: 'failed')
+
+        // Stop the entire pipeline and report an error if the build is set to
+        // FAILURE.  No sense proceeding when we know now there is a problem to
+        // fix.  This might need to change if/when the pipeline becomes
+        // parallelized.
+        if (currentBuild.result == 'FAILURE')
+        {
+          error('Build result is FAILURE')
+        }
       }
     }
   }
