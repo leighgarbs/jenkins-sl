fca7cbe4d219427af14b7ec2eaa07fa0c145597b
Leigh Garbs <leighgarbs@gmail.com>
2018-11-03 18:31:21 -0400

    WIP

--- vars/doStage.groovy
+++ vars/doStage.groovy
@@ -14,6 +14,7 @@ def call(name, body, args = [])
         }
         catch (err)
         {
+          // Any exception fails the build
           currentBuild.result = 'FAILURE'
         }
         finally
@@ -25,8 +26,13 @@ def call(name, body, args = [])
       if (currentBuild.result == 'UNSTABLE' ||
           currentBuild.result == 'FAILURE')
       {
+        // Gitlab doesn't have a commit status for unstable
         updateGitlabCommitStatus(name: name, state: 'failed')
 
+        // Stop the entire pipeline and report an error if the build is set to
+        // FAILURE.  No sense proceeding when we know now there is a problem to
+        // fix.  This might need to change if/when the pipeline becomes
+        // parallelized.
         if (currentBuild.result == 'FAILURE')
         {
           error('Build result is FAILURE')
