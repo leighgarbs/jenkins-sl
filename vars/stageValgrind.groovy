904b0631b794f1b49f306ad390495eec8b4914cd
Leigh Garbs <leighgarbs@gmail.com>
2018-11-19 12:41:36 -0600

    Initial hack at running valgrind with ctest

--- vars/stageValgrind.groovy
+++ vars/stageValgrind.groovy
@@ -2,44 +2,44 @@
 
 def call(args)
 {
-  // Run Valgrind.  Not sure if this is any better than just running Valgrind in
-  // the shell.
-  step([$class: 'ValgrindBuilder',
-    childSilentAfterFork: true,
-    excludePattern:       '',
-    generateSuppressions: false,
-    ignoreExitCode:       true,
-    includePattern:       'workdir/tests/*_test',
-    outputDirectory:      'workdir/tests',
-    outputFileEnding:     '.valgrind.xml',
-    programOptions:       '',
-    removeOldReports:     false,
-    suppressionFiles:     '',
-    tool: [$class: 'ValgrindToolMemcheck',
-          leakCheckLevel:       'full',
-          showReachable:        false,
-          trackOrigins:         true,
-          undefinedValueErrors: true],
-    traceChildren:      false,
-    valgrindExecutable: '',
-    valgrindOptions:    '',
-    workingDirectory:   'workdir/tests'])
+    runResourceScript('stageValgrind')
 
-  // Post the Valgrind analysis results to Jenkins
-  step([$class: 'ValgrindPublisher',
-    failBuildOnInvalidReports:         true,
-    failBuildOnMissingReports:         true,
-    failThresholdDefinitelyLost:       '',
-    failThresholdInvalidReadWrite:     '',
-    failThresholdTotal:                '',
-    pattern:                           'workdir/tests/*.valgrind.xml',
-    publishResultsForAbortedBuilds:    false,
-    publishResultsForFailedBuilds:     false,
-    sourceSubstitutionPaths:           '',
-    unstableThresholdDefinitelyLost:   '0',
-    unstableThresholdInvalidReadWrite: '0',
-    unstableThresholdTotal:            '0'])
+    /*step([$class: 'ValgrindBuilder',
+          childSilentAfterFork: true,
+          excludePattern:       '',
+          generateSuppressions: false,
+          ignoreExitCode:       true,
+          includePattern:       'workdir/tests/*_test',
+          outputDirectory:      'workdir/tests',
+          outputFileEnding:     '.valgrind.xml',
+          programOptions:       '',
+          removeOldReports:     false,
+          suppressionFiles:     '',
+          tool: [$class: 'ValgrindToolMemcheck',
+                 leakCheckLevel:       'full',
+                 showReachable:        false,
+                 trackOrigins:         true,
+                 undefinedValueErrors: true],
+          traceChildren:      false,
+          valgrindExecutable: '',
+          valgrindOptions:    '',
+          workingDirectory:   'workdir/tests'])*/
 
-  // This stage can't normally fail
-  return 0
+    // Post the Valgrind analysis results to Jenkins
+    step([$class: 'ValgrindPublisher',
+          failBuildOnInvalidReports:         true,
+          failBuildOnMissingReports:         true,
+          failThresholdDefinitelyLost:       '',
+          failThresholdInvalidReadWrite:     '',
+          failThresholdTotal:                '',
+          pattern:                           'workdir/tests/*.valgrind.xml',
+          publishResultsForAbortedBuilds:    false,
+          publishResultsForFailedBuilds:     false,
+          sourceSubstitutionPaths:           '',
+          unstableThresholdDefinitelyLost:   '0',
+          unstableThresholdInvalidReadWrite: '0',
+          unstableThresholdTotal:            '0'])
+
+    // This stage can't normally fail
+    return 0
 }
