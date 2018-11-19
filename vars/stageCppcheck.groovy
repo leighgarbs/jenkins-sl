169c940c5ee342adec25451d54421ee6ccd14132
Leigh Garbs <leighgarbs@gmail.com>
2018-11-19 14:44:30 -0600

    WIP

--- vars/stageCppcheck.groovy
+++ vars/stageCppcheck.groovy
@@ -2,38 +2,39 @@
 
 def call(args)
 {
-  runResourceScript('cleanUp')
+    runResourceScript('cleanUp')
 
-  def returnCode = 0
+    def returnCode = 0
 
-  dir('workdir')
-  {
-    cppcheck_args = args[0]
-    if (cppcheck_args == null)
+    dir('workdir')
     {
-      cppcheck_args = ""
+        cppcheck_args = args[0]
+        if (cppcheck_args == null)
+        {
+            cppcheck_args = ""
+        }
+
+        withEnv(['CPPCHECK_ARGS=' + cppcheck_args])
+        {
+            returnCode = runResourceScript('stageCppcheck')
+
+            if(returnCode == 1)
+            {
+                print('Setting build result to UNSTABLE')
+                currentBuild.result = 'UNSTABLE'
+            }
+
+            publishCppcheck displayAllErrors:           false,
+            displayErrorSeverity:       true,
+            displayNoCategorySeverity:  true,
+            displayPerformanceSeverity: true,
+            displayPortabilitySeverity: true,
+            displayStyleSeverity:       true,
+            displayWarningSeverity:     true,
+            pattern:                    'cppcheck-result.xml',
+            severityNoCategory:         false
+        }
     }
 
-    withEnv(['CPPCHECK_ARGS=' + cppcheck_args])
-    {
-      returnCode = runResourceScript('stageCppcheck')
-
-      if(returnCode == 1)
-      {
-        currentBuild.result = 'UNSTABLE'
-      }
-
-      publishCppcheck displayAllErrors:           false,
-                      displayErrorSeverity:       true,
-                      displayNoCategorySeverity:  true,
-                      displayPerformanceSeverity: true,
-                      displayPortabilitySeverity: true,
-                      displayStyleSeverity:       true,
-                      displayWarningSeverity:     true,
-                      pattern:                    'cppcheck-result.xml',
-                      severityNoCategory:         false
-    }
-  }
-
-  return returnCode
+    return returnCode
 }
