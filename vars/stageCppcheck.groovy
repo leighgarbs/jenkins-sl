f4df61fc588ccf697722b42832a60a5874eb5993
Leigh Garbs <leighgarbs@gmail.com>
2018-11-19 16:39:29 -0600

    WIP

--- vars/stageCppcheck.groovy
+++ vars/stageCppcheck.groovy
@@ -2,11 +2,14 @@
 
 def call(args)
 {
+    // Go back to a pristine checkout
     runResourceScript('cleanUp')
 
     def returnCode = 0
 
-    dir('workdir')
+    // stageCppcheck script is written to work out of the current directory, and
+    // all the code is in STAGE_DIR, so go there
+    dir(STAGE_DIR)
     {
         cppcheck_args = args[0]
         if (cppcheck_args == null)
@@ -17,24 +20,27 @@ def call(args)
         withEnv(['CPPCHECK_ARGS=' + cppcheck_args])
         {
             returnCode = runResourceScript('stageCppcheck')
-
-            if(returnCode == 1)
-            {
-                print('Setting build result to UNSTABLE')
-                currentBuild.result = 'UNSTABLE'
-            }
-
-            publishCppcheck displayAllErrors:           false,
-            displayErrorSeverity:       true,
-            displayNoCategorySeverity:  true,
-            displayPerformanceSeverity: true,
-            displayPortabilitySeverity: true,
-            displayStyleSeverity:       true,
-            displayWarningSeverity:     true,
-            pattern:                    'cppcheck-result.xml',
-            severityNoCategory:         false
         }
     }
+
+    // The cppcheck stage returns non-zero if it finds an issue with a severity
+    // of either warning or error
+    if(returnCode == 1)
+    {
+        print('Setting build result to UNSTABLE')
+        currentBuild.result = 'UNSTABLE'
+    }
+
+    // Publish all the results to the Jenkins plugin
+    publishCppcheck displayAllErrors: false,
+    displayErrorSeverity:             true,
+    displayNoCategorySeverity:        true,
+    displayPerformanceSeverity:       true,
+    displayPortabilitySeverity:       true,
+    displayStyleSeverity:             true,
+    displayWarningSeverity:           true,
+    pattern:                          'cppcheck.xml',
+    severityNoCategory:               false
 
     return returnCode
 }
