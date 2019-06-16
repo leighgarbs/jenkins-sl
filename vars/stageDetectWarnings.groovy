1721d20d0dfec860005eee05b28586274d90dee8
Leigh Garbs <leighgarbs@gmail.com>
2019-06-15 21:04:49 -0700

    spacing

--- vars/stageDetectWarnings.groovy
+++ vars/stageDetectWarnings.groovy
@@ -2,7 +2,7 @@
 
 def call(args)
 {
-    recordIssues qualityGates: [[threshold: 1, type: 'TOTAL', unstable:  true]],
+    recordIssues qualityGates: [[threshold: 1, type: 'TOTAL', unstable: true]],
                  tools: [gcc(), clang()]
 
     // This stage should not fail for build warnings (the only
