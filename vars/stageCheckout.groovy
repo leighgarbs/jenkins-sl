a6799e805f366b660ab675471a0fe2d682a1e412
Leigh Garbs <leighgarbs@gmail.com>
2019-06-18 21:32:06 -0700

    WIP

--- vars/stageCheckout.groovy
+++ vars/stageCheckout.groovy
@@ -2,24 +2,29 @@
 
 def call(args)
 {
-  // This clone the git repository we'll be building and testing.  Not sure if
-  // this is somehow better than just doing the clone in the shell.
-  checkout changelog: true, poll: true, scm: [$class: 'GitSCM',
-    branches: [[name: env.BRANCH_NAME]],
-    browser: [$class: 'GitLab',
-             repoUrl: args[0],
-             version: '$GITLAB_VERSION'],
-    extensions: [[$class: 'SubmoduleOption',
-                disableSubmodules:   false,
-                parentCredentials:   false,
-                recursiveSubmodules: true,
-                reference:           '',
-                trackingSubmodules:  false],
-                [$class: 'RelativeTargetDirectory',
-                relativeTargetDir: 'workdir']],
-    submoduleCfg: [],
-    userRemoteConfigs: [[credentialsId: '', url: args[0]]]]
+    // Clone the repository containing the code to test.  Not sure if this is
+    // somehow better than just doing the clone in the shell.
+    checkout changelog: true,
+             poll: true,
+             scm: [$class: 'GitSCM',
+                   branches: [[name: env.BRANCH_NAME]],
+                   browser: [$class: 'GitLab',
+                              repoUrl: args[0],
+                              version: '$GITLAB_VERSION'],
+                   extensions: [[$class: 'SubmoduleOption',
+                                 disableSubmodules: false,
+                                 parentCredentials: false,
+                                 recursiveSubmodules: true,
+                                 reference: '',
+                                 trackingSubmodules: false],
+                                [$class: 'RelativeTargetDirectory',
+                                 relativeTargetDir: 'workdir']],
+                   submoduleCfg: [],
+                   userRemoteConfigs: [[credentialsId: '', url: args[0]]]]
 
-  // This repository has some scripts we need for later stages
-  sh returnStatus: true, script: 'git clone http://gitlab.dmz/leighgarbs/bin.git'
+    // Check out any other repositories we need
+    for (repoUrl in args[1])
+    {
+        sh returnStatus: true, script: 'git clone ' + repoUrl
+    }
 }
