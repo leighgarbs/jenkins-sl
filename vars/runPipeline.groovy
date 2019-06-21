96896d8bf3b5666649af2c48b116bd59290dd8d6
Leigh Garbs <leighgarbs@gmail.com>
2019-06-20 21:24:25 -0700

    Manual merge of new pipeline and stage execution code with old code

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -1,38 +1,57 @@
-#!groovy                                                                            
+#!groovy
 
 def call(pipeline, platform)
 {
-    // Allocate a node for this whole pipeline.  Entire pipelines get assigned      
-    // to nodes this way.  It is possible to assign workflow to nodes at an         
-    // increased resolution (such as assigning individual stages to nodes) but      
-    // this seems sufficient for now.                                               
+    // We have to have a list of all the stage names before we run any of the
+    // stages.  This is a little awkward since it forces us to loop
+    // over the same list of stages twice.
+    stageNames = getStageNames(pipeline)
 
-    // This assumes nodes are labeled with the name of the platform they run.       
-    // Labeling nodes can be done using the Jenkins GUI under "Manage Jenkins ->    
-    // Manage Nodes".                                                               
+    properties([[$class: 'GitLabConnectionProperty',
+                 gitLabConnection: 'gitlab.dmz'],
+                pipelineTriggers([[$class: 'GitLabPushTrigger',
+                                   triggerOnPush: true,
+                                   triggerOnMergeRequest: true,
+                                   skipWorkInProgressMergeRequest: true,
+                                   pendingBuildName: stageNames[0]]])])
 
-    node(platform)
+    // This tells Gitlab the names of the stages we'll be running
+    gitlabBuilds(builds: stageNames)
     {
-        timestamps
+        // Allocate a node for this whole pipeline.  Entire pipelines get
+        // assigned to nodes this way.  It is possible to assign workflow to
+        // nodes at an increased resolution (such as assigning individual stages
+        // to nodes) but this seems sufficient for now.
+
+        // This assumes nodes are labeled with the name of the platform they
+        // run.  Labeling nodes can be done using the Jenkins GUI under "Manage
+        // Jenkins -> Manage Nodes".
+
+        node(platform)
         {
-            for (def i = 0; i < pipeline.size(); i++)
+            timestamps
             {
-                print 'Running stage ' + pipeline[i].name + ' on ' + platform
-
-                // This does return a usable error code but runStage() should       
-                // have dealt with handling the error already.                      
-                runStage(pipeline[i])
-
-                // Doing this causes this build to stop at the first stage that     
-                // is unstable or has outright failed.  Otherwise the stage will    
-                // be marked as unstable or failed and the build will continue.     
-                if (currentBuild.result == 'UNSTABLE' ||
-                    currentBuild.result == 'FAILURE')
+                for (def i = 0; i < pipeline.size(); i++)
                 {
-                    print 'Current build result is ' + currentBuild.result +
-                        ', exiting early'
+                    print 'Running stage ' + pipeline[i].name + ' on ' +
+                        platform
 
-                    break
+                    // This does return a usable error code but runStage()
+                    // should have dealt with handling the error already.
+                    runStage(pipeline[i])
+
+                    // Doing this causes this build to stop at the first stage
+                    // that is unstable or has outright failed.  Otherwise the
+                    // stage will be marked as unstable or failed and the build
+                    // will continue.
+                    if (currentBuild.result == 'UNSTABLE' ||
+                        currentBuild.result == 'FAILURE')
+                    {
+                        print 'Current build result is ' + currentBuild.result +
+                            ', exiting early'
+
+                        break
+                    }
                 }
             }
         }
