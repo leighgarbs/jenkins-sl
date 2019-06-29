92f39dbdc4ecc8657b8ca74ce034be65ce4be414
Leigh Garbs <leighgarbs@gmail.com>
2019-06-29 15:02:20 -0700

    WIP

--- vars/Stage.groovy
+++ vars/Stage.groovy
@@ -2,17 +2,7 @@
 
 abstract class Stage
 {
-    // Intended to store a reference to an implicitly-defined binding object
-    // when this class is used from a script
-    def binding
+    String name
 
-    String    name
-    ArrayList arguments
-
-    abstract boolean body(ArrayList arguments)
-
-    boolean run()
-    {
-        return body(arguments)
-    }
+    abstract boolean run()
 }
--- vars/stageCheckout.groovy
+++ vars/stageCheckout.groovy
@@ -1,25 +1,12 @@
 #!groovy
 
-def call(args)
+class StageCheckout extends Stage
 {
-  // This clone the git repository we'll be building and testing.  Not sure if
-  // this is somehow better than just doing the clone in the shell.
-  checkout changelog: true, poll: true, scm: [$class: 'GitSCM',
-    branches: [[name: env.BRANCH_NAME]],
-    browser: [$class: 'GitLab',
-             repoUrl: args[0],
-             version: '$GITLAB_VERSION'],
-    extensions: [[$class: 'SubmoduleOption',
-                disableSubmodules:   false,
-                parentCredentials:   false,
-                recursiveSubmodules: true,
-                reference:           '',
-                trackingSubmodules:  false],
-                [$class: 'RelativeTargetDirectory',
-                relativeTargetDir: 'workdir']],
-    submoduleCfg: [],
-    userRemoteConfigs: [[credentialsId: '', url: args[0]]]]
+    ArrayList repos
 
-  // This repository has some scripts we need for later stages
-  sh returnStatus: true, script: 'git clone http://gitlab.dmz/leighgarbs/bin.git'
+    boolean run()
+    {
+        print binding.currentBuild.result
+        print repos
+    }
 }
