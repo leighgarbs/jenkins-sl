aee3e8a2887d572ac3fe2695c9ceb86b5c0b9ef6
Leigh Garbs <leighgarbs@gmail.com>
2019-06-28 23:02:11 -0700

    WIP

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -2,6 +2,11 @@
 
 class Checkout extends Stage
 {
+    Checkout(binding)
+    {
+        this.binding = binding
+    }
+
     boolean body(ArrayList arguments)
     {
         print binding.currentBuild.result
