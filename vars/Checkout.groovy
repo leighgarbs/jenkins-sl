401b7a5b35e7daf4d0e2e479261cb2122f38a87a
Leigh Garbs <leighgarbs@gmail.com>
2019-06-28 22:24:18 -0700

    WIP

--- vars/Checkout.groovy
+++ vars/Checkout.groovy
@@ -3,6 +3,10 @@
 class Checkout extends Stage
 {
     Checkout(name = 'CHECKOUT', arguments = [])
+    {
+        this.name = name
+        this.arguments = arguments
+    }
 
     boolean body(ArrayList arguments)
     {
