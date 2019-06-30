6c49cce2a7b6d9fc44cf1205de7819f6fa97e214
Leigh Garbs <leighgarbs@gmail.com>
2019-06-29 20:52:31 -0700

    WIP

--- src/stage/StageCheckout.groovy
+++ src/stage/StageCheckout.groovy
@@ -4,21 +4,57 @@ package stage
 
 class StageCheckout extends Stage
 {
-    ArrayList<String> repos
+    String            repo_under_test
+    ArrayList<String> other_repos
 
     // Constructor
     StageCheckout(def               wfc,
-                  ArrayList<String> repos,
+                  String            repo_under_test,
+                  ArrayList<String> other_repos,
                   String            name = 'CHECKOUT')
     {
         // Satisfy the parent constructor
         super(wfc, name)
 
-        this.repos = repos
+        this.repo_under_test = repo_under_test
+        this.other_repos = other_repos
     }
 
     boolean body()
     {
+        // This clone the git repository we'll be building and testing.  Not
+        // sure if this is somehow better than just doing the clone in the
+        // shell.
+        wfc.checkout changelog: true,
+                     poll: true,
+                     scm: [$class: 'GitSCM',
+                           branches: [[name: env.BRANCH_NAME]],
+                           browser: [$class: 'GitLab',
+                                     repoUrl: repo_under_test,
+                                     version: '$GITLAB_VERSION'],
+                           extensions: [[$class: 'SubmoduleOption',
+                                         disableSubmodules:   false,
+                                         parentCredentials:   false,
+                                         recursiveSubmodules: true,
+                                         reference:           '',
+                                         trackingSubmodules:  false],
+                                        [$class: 'RelativeTargetDirectory',
+                                         relativeTargetDir: 'workdir']],
+                           submoduleCfg: [],
+                           userRemoteConfigs: [[credentialsId: '',
+                                                url: repo_under_test]]]
+
+        // These repositories have things we need for other stages, but aren't
+        // themselves under test
+        for (repo in other_repos)
+        {
+            // Fail if check out does not work
+            if (sh returnStatus: true, script: repo != 0)
+            {
+                return false
+            }
+        }
+
         return true
     }
 }
