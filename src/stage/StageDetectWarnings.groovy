9a95f7889a24d3d773abbdaf0c9e3b5d5c9bceb4
Leigh Garbs <leighgarbs@gmail.com>
2019-06-30 10:40:53 -0700

    WIP

--- src/stage/StageDetectWarnings.groovy
+++ src/stage/StageDetectWarnings.groovy
@@ -21,7 +21,8 @@ class StageDetectWarnings extends Stage
                                          unstable: true]],
                          tools: [wfc.gcc(), wfc.clang()]
 
-        wfc.scanForIssues tool: wfc.cppCheck(pattern: 'cppcheck.xml')
+        wfc.scanForIssues tool: wfc.cppCheck(pattern: wfc.STAGE_DIR +
+                                             '/cppcheck.xml')
 
         // This stage should not fail for build warnings (the only
         // failures would be internal to the analysis tools)
