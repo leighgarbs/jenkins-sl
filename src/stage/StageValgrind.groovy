3137644287ad83b278eb4a268cb256742343e80f
Leigh Garbs <leighgarbs@gmail.com>
2019-07-01 20:02:53 -0700

    WIP

--- src/stage/StageValgrind.groovy
+++ src/stage/StageValgrind.groovy
@@ -18,8 +18,8 @@ class StageValgrind extends Stage
         // if it's necessary to do so.
         wfc.runResourceScript('stageValgrind')
 
-        echo 'BEFORE'
-        echo wfc.currentBuild.result
+        wfc.echo 'BEFORE'
+        wfc.echo wfc.currentBuild.result
 
         // Post the Valgrind analysis results to Jenkins.  This will fail the
         // build if there are no Valgrind reports or if the reports are
@@ -35,8 +35,8 @@ class StageValgrind extends Stage
                   publishResultsForFailedBuilds: false,
                   sourceSubstitutionPaths: ''])
 
-        echo 'AFTER'
-        echo wfc.currentBuild.result
+        wfc.echo 'AFTER'
+        wfc.echo wfc.currentBuild.result
 
         // This stage fails by setting currentBuild, not by return code
         return true
