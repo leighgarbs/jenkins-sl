cad274de790cdbd1ec4ec0236ba42eb90dd3f8de
Leigh Garbs <leighgarbs@gmail.com>
2019-07-01 21:08:21 -0700

    WIP

--- src/stage/StageClangStaticAnalysis.groovy
+++ src/stage/StageClangStaticAnalysis.groovy
@@ -14,7 +14,7 @@ class StageClangStaticAnalysis extends Stage
     boolean body()
     {
         def asdf = wfc.runResourceScript('stageClangStaticAnalysis')
-        wfc.echo(asdf)
+        wfc.echo(String(asdf))
         return asdf == 0
     }
 }
