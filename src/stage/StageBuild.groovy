5be003c2f54b2178a70d9748525028c9993be733
Leigh Garbs <leighgarbs@gmail.com>
2019-07-27 14:43:40 -0700

    WIP

--- src/stage/StageBuild.groovy
+++ src/stage/StageBuild.groovy
@@ -10,24 +10,17 @@ class StageBuild extends Stage
     // Make target
     String target
 
-    // Should this stage publish build warnings?  Build warnings can only be
-    // published once per pipeline.  Trying to publish them more than once will
-    // crash the pipeline.
-    boolean recordIssues
-
     // Constructor
     StageBuild(def     wfc,
                String  name,
                String  buildType,
-               String  target,
-               boolean recordIssues)
+               String  target)
     {
         // Satisfy the parent constructor
         super(wfc, name)
 
         this.buildType = buildType
         this.target = target
-        this.recordIssues = recordIssues
     }
 
     boolean body()
@@ -35,24 +28,15 @@ class StageBuild extends Stage
         // Use environment variables to get data into the resource script
         wfc.withEnv(['BUILD_TYPE=' + buildType, 'TARGET=' + target])
         {
+            // Actually run the build.  This tees output to a file that is used
+            // by the reporting step below
             def returnCode = wfc.runResourceScript('stageBuild')
 
-            //def scanResult = wfc.scanForIssues tool: wfc.gcc(
-            //    pattern: 'make.' + buildType + '.out'),
-            //id: 'gcc-' + buildType
-
-            // Publish build warnings
-            //wfc.recordIssues issues: [scanResult],
-            //id: 'gcc-' + buildType,
-            //enabledForFailure: true,
-            //qualityGates: [[threshold: 1,
-            //                type: 'TOTAL',
-            //                unstable: false]]
-
-            wfc.recordIssues aggregatingResults: true,
-            qualityGates: [[threshold: 1,
-                            type: 'TOTAL',
-                            unstable: false]],
+            // Report any build warnings.  This should fail the build if any
+            // are discovered.
+            wfc.recordIssues qualityGates: [[threshold: 1,
+                                             type: 'TOTAL',
+                                             unstable: false]],
             tools: [wfc.gcc(id: 'gcc-' + buildType,
                             name: 'Hello' + buildType,
                             pattern: 'make.' + buildType + '.out')]
