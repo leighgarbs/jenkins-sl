2166b521646b6212f5f4ed102b1841769b33f611
Leigh Garbs <leighgarbs@gmail.com>
2019-07-31 21:21:19 -0700

    WIP

--- src/stage/StageBuild.groovy
+++ src/stage/StageBuild.groovy
@@ -25,6 +25,7 @@ class StageBuild extends Stage
         // Use environment variables to get data into the resource script
         wfc.withEnv(['BUILD_TYPE=' + buildType, 'TARGET=' + target])
         {
+            sh 'echo $PWD'
             def returnCode = wfc.runResourceScript(wfc, 'linux/stageBuild')
 
             // Make how the build warnings display in the GUI a bit prettier
