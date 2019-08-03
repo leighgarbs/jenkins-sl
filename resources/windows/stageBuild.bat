b37b53bd1217cd811af42572eb42268f3d9bcc98
Leigh Garbs <leighgarbs@gmail.com>
2019-08-02 20:18:01 -0700

    WIP

--- resources/windows/stageBuild.bat
+++ resources/windows/stageBuild.bat
@@ -6,5 +6,6 @@ call "%VSDEVCMD%"
 REM We always want a clean build
 git clean -x -d -f
 
-REM The single carat capitalizes the first letter of the BUILD_TYPE string
 cmake -DCMAKE_BUILD_TYPE=Debug .
+
+make "%TARGET%"
