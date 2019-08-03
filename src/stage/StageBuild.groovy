d3d231d23a014fa935410865eea19c45541411a0
Leigh Garbs <leighgarbs@gmail.com>
2019-08-03 13:40:24 -0700

    WIP

--- src/stage/StageBuild.groovy
+++ src/stage/StageBuild.groovy
@@ -44,7 +44,8 @@ class StageBuild extends Stage
 
         // Report any build warnings.  This should fail the build if any
         // are discovered.
-        wfc.recordIssues enabledForFailure: true,
+        wfc.recordIssues filters: [wfc.includeFile(wfc.env.WORKSPACE)],
+        enabledForFailure: true,
         qualityGates: [[threshold: 1,
                         type: 'TOTAL',
                         unstable: false]],
