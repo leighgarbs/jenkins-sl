9daa0f1f8ca478a40739e703c48066be190c133e
Leigh Garbs <leighgarbs@gmail.com>
2019-08-03 10:16:59 -0700

    WIP

--- src/stage/StageBuild.groovy
+++ src/stage/StageBuild.groovy
@@ -82,7 +82,7 @@ class StageBuild extends Stage
 
         // Include only warnings originating from code in the workspace.
         // Assumes we don't care about anything else.
-        def excludeFilter = '.*'
+        def excludeFilter = ''
         def includeFilter = wfc.env.WORKSPACE.replace('\\', '\\\\') + '.*'
         //def excludeFilter = ''
         //def includeFilter = 'C:\\Users.*'
