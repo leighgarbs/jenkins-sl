359657faa22c19a73ddb50435e1ef53f6f5ea9fd
Leigh Garbs <leighgarbs@gmail.com>
2019-08-03 20:40:40 -0700

    StageCheckout cleaned up

--- src/stage/StageCheckout.groovy
+++ src/stage/StageCheckout.groovy
@@ -6,9 +6,6 @@ class StageCheckout extends Stage
 {
     String git_repo
 
-    // Some of the stages use utilities out of this repository
-    def binRepo = 'git clone http://gitlab.dmz/leighgarbs/bin.git'
-
     // Constructor
     StageCheckout(def wfc,
                   String git_repo,
@@ -24,29 +21,19 @@ class StageCheckout extends Stage
 
     boolean runLinux()
     {
-        // This clones the git repository we'll be building and testing.  Not
-        // sure if this is somehow better than just doing the clone in the
-        // shell.
-        wfc.checkout changelog: true,
-        poll: true,
-        scm: [$class: 'GitSCM',
-              branches: [[name: wfc.env.BRANCH_NAME]],
-              browser: [$class: 'GitLab',
-                        repoUrl: git_repo,
-                        version: '$GITLAB_VERSION'],
-              extensions: [[$class: 'SubmoduleOption',
-                            disableSubmodules: false,
-                            parentCredentials: false,
-                            recursiveSubmodules: true,
-                            reference: '',
-                            trackingSubmodules: false]],
-              submoduleCfg: [],
-              userRemoteConfigs: [[credentialsId: '', url: git_repo]]]
-
-        return true
+        // Nothing platform-specific about this stage so we define it in once
+        // place
+        return runAllPlatforms()
     }
 
     boolean runWindows()
+    {
+        // Nothing platform-specific about this stage so we define it in once
+        // place
+        return runAllPlatforms()
+    }
+
+    boolean runAllPlatforms()
     {
         // This clones the git repository we'll be building and testing.  Not
         // sure if this is somehow better than just doing the clone in the
