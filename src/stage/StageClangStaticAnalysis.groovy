e0ba3d7c7ff4c82714c45b6b0eaba9bfe420a64a
Leigh Garbs <leighgarbs@gmail.com>
2019-12-24 13:04:39 -0600

    Copied from the build stage

--- src/stage/StageClangStaticAnalysis.groovy
+++ src/stage/StageClangStaticAnalysis.groovy
@@ -25,8 +25,14 @@ class StageClangStaticAnalysis extends Stage
         def returnCode =
             wfc.runResourceScript(wfc, 'linux/stageClangStaticAnalysis')
 
-        // Publish build warnings
-        wfc.recordIssues enabledForFailure: true,
+        // Include only warnings originating from code in the workspace.  The
+        // workspace pattern has to be matched case insensitive because Jenkins
+        // stores the pattern in all lowercase for some reason.  This seems like
+        // something a user could maybe want to configure but for now just
+        // hardcode it.
+        wfc.recordIssues filters:
+            [wfc.includeFile('(?i)^' + wfc.env.WORKSPACE)],
+        enabledForFailure: true,
         qualityGates: [[threshold: 1,
                         type: 'TOTAL',
                         unstable: false]],
